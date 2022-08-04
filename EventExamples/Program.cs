3bdf80cde847568b8d7d0c4dbea4a294d29f1000
Alexander Dolgosheev <izumroot@deeplay.io>
2022-08-05 00:30:03 +0600

    code beauty

---
+++ EventExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Events.Classes;
+using Events.Interfaces;
+
+namespace Events
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Humans.HumanEditions += Human_HumanEditions;
+            Humans.HumanCreated += Humans_HumanCreated;
+
+            IHuman men = new Mens("Alexander", "Dolgosheev", 33);
+            IHuman women = new Womens("Marina", "Dolgosheeva", 32);
+
+            men.PlusYearAge();
+            women.PlusYearAge();
+
+            Console.ReadKey();
+        }
+
+        private static void Humans_HumanCreated(object sender, IHuman e)
+        {
+            Console.WriteLine($"Created {e.Fname} {e.Sname} age {e.Age}");
+        }
+
+        private static void Human_HumanEditions(object sender, IHuman e)
+        {
+            Console.WriteLine($"Age for {e.Fname} {e.Sname} has been changed to {e.Age}");
+        }
+    }
+}
