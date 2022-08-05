dfa9a4f0940111b16d34a995db12cc23f2f8d944
Alexander Dolgosheev <izumroot@deeplay.io>
2022-08-05 18:53:21 +0600

    refactored

--- NetworkInfo/Program.cs
+++ NetworkInfo/Program.cs
@@ -1,63 +1,32 @@
 ﻿using System;
+using System.Linq;
 using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
 
-namespace networkAdapter
+var properties = IPGlobalProperties.GetIPGlobalProperties();
+var interfaces = NetworkInterface.GetAllNetworkInterfaces().ToList();
+
+Console.WriteLine("IPv4 interface information for {0}.{1}\nFound : {2} interfaces\n", properties.HostName,
+    properties.DomainName, interfaces.Count);
+
+interfaces.ForEach(adapter =>
 {
-    internal class Program
+    if (adapter is null) return;
+
+    var result = new StringBuilder();
+    result.AppendLine($"\t{"Name",-12} : {adapter.Name}");
+    result.AppendLine($"\t{"Description",-12} : {adapter.Description}");
+    result.AppendLine($"\t{"Status",-12} : {adapter.OperationalStatus}");
+
+    adapter.GetIPProperties().UnicastAddresses.ToList().ForEach(ip =>
     {
-        private static void Main()
-        {
+        if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+            result.AppendLine($"\t{"IP",-12} : {ip.Address}");
+    });
+    result.AppendLine($"\t{"Number",-12} : {adapter.GetIPProperties().GetIPv4Properties().Index}");
 
-            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+    Console.WriteLine(result);
+});
 
-            Console.WriteLine("IPv4 interface information for {0}.{1}\n", properties.HostName, properties.DomainName);
-
-            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
-            foreach (NetworkInterface network in networkInterfaces)
-            {
-                PrintInterfaceIndex(network.Description);
-            }
-
-            Console.ReadKey();
-        }
-
-        private static void PrintInterfaceIndex(string adapterName)
-        {
-            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
-
-            foreach (NetworkInterface adapter in nics)
-            {
-                if (adapter.Supports(NetworkInterfaceComponent.IPv4) == false)
-                {
-                    continue;
-                }
-
-                if (!adapter.Description.Equals(adapterName, StringComparison.OrdinalIgnoreCase))
-                {
-                    continue;
-                }
-                Console.WriteLine("\tName        : {0}", adapter.Description);
-                Console.WriteLine("\tDescription : {0}", adapter.Name);
-                Console.WriteLine("\tStatus      : {0}", adapter.OperationalStatus);
-
-                foreach (UnicastIPAddressInformation ip in adapter.GetIPProperties().UnicastAddresses)
-                {
-                    if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
-                    {
-                        Console.WriteLine("\tIP          : {0}", ip.Address);
-                    }
-                }
-
-                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
-                IPv4InterfaceProperties p = adapterProperties.GetIPv4Properties();
-
-                if (p == null)
-                {
-                    Console.WriteLine("No information is available for this interface.");
-                    continue;
-                }
-                Console.WriteLine("\tNumber       : {0}\n", p.Index);
-            }
-        }
-    }
-}
+Console.ReadKey();
